0d7a0f03f5357d5abe8e5d9a9f795c0ceb4832b2
Sup <dinhthilannb1983@gmail.com>
2025-05-13 17:33:00 +0700

    Add files via upload

---
+++ aarch64.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Setting the chroot directory
+CHROOT_DIR="/tmp/alpine-aarch64"
+ALPINE_TAR="alpine-minirootfs-3.21.3-aarch64.tar.gz"
+ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/aarch64/$ALPINE_TAR"
+
+# Check if the chroot folder exists, if not, create it
+if [ ! -d "$CHROOT_DIR" ]; then
+    echo "Creating directory $CHROOT_DIR..."
+    mkdir -p "$CHROOT_DIR"
+fi
+
+# Download Alpine if the file does not exist
+if [ ! -f "$CHROOT_DIR/$ALPINE_TAR" ]; then
+    echo "Downloading Alpine Linux for aarch64..."
+    wget -O "$CHROOT_DIR/$ALPINE_TAR" "$ALPINE_URL"
+fi
+
+# Extract Alpine if it is not already extracted
+if [ ! -d "$CHROOT_DIR/bin" ]; then
+    echo "Extracting Alpine Linux..."
+    tar -xzf "$CHROOT_DIR/$ALPINE_TAR" -C "$CHROOT_DIR"
+    # Remove tar file after extraction
+    echo "Removing file $ALPINE_TAR..."
+    rm -f "$CHROOT_DIR/$ALPINE_TAR"
+
+fi
+
+# Mount the necessary file systems
+echo "Mounting filesystems..."
+mount -t proc /proc "$CHROOT_DIR/proc"
+mount --rbind /sys "$CHROOT_DIR/sys"
+mount --rbind /dev "$CHROOT_DIR/dev"
+
+# Enter chroot
+echo "Entering chroot..."
+exec chroot "$CHROOT_DIR" /bin/sh
